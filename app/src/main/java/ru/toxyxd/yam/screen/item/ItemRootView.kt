fa056bae404f2b4155b4274e85fc4177b8da5a6e
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2023-07-30 16:00:20 +0300

    refactor: replace m2 with m3 imports

--- app/src/main/java/ru/toxyxd/yam/screen/item/ItemRootView.kt
+++ app/src/main/java/ru/toxyxd/yam/screen/item/ItemRootView.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.IconButton
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Card

dd5df800f5f4e816ffd63f60923dcb5d5bdda31c
aliveoutside <53598473+aliveoutside@users.noreply.github.com>
2023-10-20 11:17:35 +0300

    fix: change bottom content padding based on visibility of player

--- app/src/main/java/ru/toxyxd/yam/ContentView.kt
+++ app/src/main/java/ru/toxyxd/yam/ContentView.kt
@@ -1,14 +1,13 @@
 package ru.toxyxd.yam
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.jetpack.stack.Children
 import com.arkivanov.decompose.extensions.compose.jetpack.stack.animation.fade
@@ -47,11 +46,12 @@ private fun Content(
             collapsedBound = miniPlayerHeight,
             expandedBound = maxHeight,
         )
+        val sheetPadding = playerBottomSheetState.value.coerceAtMost(56.dp)
 
         Scaffold { paddings ->
             Children(
                 stack = component.childStack,
-                modifier = Modifier.padding(paddings).padding(bottom = miniPlayerHeight),
+                modifier = Modifier.padding(paddings).padding(bottom = sheetPadding),
                 animation = stackAnimation(fade() + scale())
             ) {
                 when (val child = it.instance) {
